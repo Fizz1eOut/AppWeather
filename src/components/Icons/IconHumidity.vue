e2e6993e58bfcacc602350794126323e8c8dad3d
FizzleOut <fizzleout1337@gmail.com>
2024-06-02 16:08:34 +0200

    App Icons Components

---
+++ src/components/Icons/IconHumidity.vue
@@ -0,0 +1,152 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IconCompass',
+  
+});
+</script>
+
+<template>
+  <svg
+    viewBox="0 0 114 138"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_30_191)">
+      <g filter="url(#filter0_bd_30_191)">
+        <path
+          d="M30 4C30 4 8.38348 28.2437 8.38348 42.3835C8.38348 54.322 18.0615 64 30 64C41.9385 64 51.6165 54.322 51.6165 42.3835C51.6165 28.2437 30 4 30 4ZM34.3183 56.4552C40.0765 54.0459 44.1835 47.7634 44.1835 40.3898C44.1835 34.8836 40.5379 27.4453 36.8335 21.3048C40.5425 26.6941 46.2331 35.9834 46.2331 42.2539C46.2329 49.8745 40.9504 56.0944 34.3183 56.4552Z"
+          fill="#1F8CC9"
+          fill-opacity="0.8"
+          shape-rendering="crispEdges"
+        />
+      </g>
+    </g>
+    <g filter="url(#filter1_bd_30_191)">
+      <path
+        d="M86 28C86 28 71.589 44.1625 71.589 53.589C71.589 61.548 78.041 68 86 68C93.959 68 100.411 61.548 100.411 53.589C100.411 44.1625 86 28 86 28ZM88.8789 62.9701C92.7177 61.3639 95.4557 57.1756 95.4557 52.2599C95.4557 48.5891 93.0253 43.6302 90.5557 39.5365C93.0283 43.1294 96.8221 49.3222 96.8221 53.5026C96.8219 58.583 93.3003 62.7296 88.8789 62.9701Z"
+        fill="#1F8CC9"
+        fill-opacity="0.8"
+        shape-rendering="crispEdges"
+      />
+    </g>
+    <g clip-path="url(#clip1_30_191)">
+      <g filter="url(#filter2_bd_30_191)">
+        <path
+          d="M59 48C59 48 30.178 80.325 30.178 99.178C30.178 115.096 43.082 128 59 128C74.918 128 87.822 115.096 87.822 99.178C87.822 80.325 59 48 59 48ZM64.7578 117.94C72.4353 114.728 77.9114 106.351 77.9114 96.5198C77.9114 89.1782 73.0505 79.2604 68.1113 71.073C73.0566 78.2588 80.6441 90.6445 80.6441 99.0052C80.6439 109.166 73.6006 117.459 64.7578 117.94Z"
+          fill="#1F8CC9"
+          fill-opacity="0.8"
+          shape-rendering="crispEdges"
+        />
+      </g>
+    </g>
+    <defs>
+      <filter
+        id="filter0_bd_30_191"
+        x="-1.61652"
+        y="-6"
+        width="63.233"
+        height="80"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feGaussianBlur in="BackgroundImageFix" stdDeviation="2" />
+        <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_30_191" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset />
+        <feGaussianBlur stdDeviation="5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.3 0" />
+        <feBlend mode="normal" in2="effect1_backgroundBlur_30_191" result="effect2_dropShadow_30_191" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect2_dropShadow_30_191"
+          result="shape"
+        />
+      </filter>
+      <filter
+        id="filter1_bd_30_191"
+        x="61.589"
+        y="18"
+        width="48.822"
+        height="60"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feGaussianBlur in="BackgroundImageFix" stdDeviation="2" />
+        <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_30_191" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset />
+        <feGaussianBlur stdDeviation="5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.3 0" />
+        <feBlend mode="normal" in2="effect1_backgroundBlur_30_191" result="effect2_dropShadow_30_191" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect2_dropShadow_30_191"
+          result="shape"
+        />
+      </filter>
+      <filter
+        id="filter2_bd_30_191"
+        x="20.178"
+        y="38"
+        width="77.644"
+        height="100"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feGaussianBlur in="BackgroundImageFix" stdDeviation="2" />
+        <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_30_191" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset />
+        <feGaussianBlur stdDeviation="5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.3 0" />
+        <feBlend mode="normal" in2="effect1_backgroundBlur_30_191" result="effect2_dropShadow_30_191" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect2_dropShadow_30_191"
+          result="shape"
+        />
+      </filter>
+      <clipPath id="clip0_30_191">
+        <rect width="60" height="80" fill="white" />
+      </clipPath>
+      <clipPath id="clip1_30_191">
+        <rect
+          width="81"
+          height="97"
+          fill="white"
+          transform="translate(19 41)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
+
+<style scoped>
+
+</style>
