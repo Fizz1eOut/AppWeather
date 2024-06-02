c7cf487d8247b1130cbe53a4081c2f0576d05eb3
FizzleOut <fizzleout1337@gmail.com>
2024-06-02 16:16:02 +0200

    A main page has been created on which we request an API key to display data. And a method has also been created to display a picture using data from api-key

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,11 +1,79 @@
 <script>
+import { API_KEY, BASE_URL } from '@/assets/api/script';
+import { defineComponent } from 'vue';
 
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+
+  },
+
+  data() {
+    return {
+      city: 'New York',
+      weatherInfo: null
+    };
+  },
+
+  mounted() {
+    this.getWeather();
+  },
+
+  methods: {
+    async getWeather() {
+      try {
+        const response = await fetch(`${BASE_URL}?q=${this.city}&units=metric&appid=${API_KEY}`);
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+        this.weatherInfo = data;
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    },
+
+    getWeatherIcon(description) {
+      // Заменяем пробелы на %20
+      const formattedDescription = description.replace(/ /g, '%20');
+      // Формируем путь к изображению на основе данных
+      return `src/assets/img/background/${formattedDescription}.png`;
+    }
+  }
+});
 </script>
 
 <template>
-  home
+  <div class="weather">
+    <div v-if="weatherInfo" class="weather-background">
+      <img :src="getWeatherIcon(weatherInfo.weather[0].description)" alt="Icon" class="background-image">
+    </div>
+  </div>
 </template>
 
-<script>
+<style scoped>
+  .weather {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
 
-</script>
+.weather-background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.background-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
