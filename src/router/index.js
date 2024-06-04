8da89e196d85bb9d4a7325d5713d790791a71852
FizzleOut <fizzleout1337@gmail.com>
2024-06-04 19:32:17 +0200

    fixed vite

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory} from 'vue-router';
 import HomeView from '@/pages/HomeView.vue';
 
 export const router = createRouter({
-  history: createWebHashHistory('/'),
+  history: createWebHashHistory('/AppWeather'),
   routes: [
     { path: '/', component: HomeView, name: 'HomeView'},
   ],
